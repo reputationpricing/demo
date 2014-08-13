c0813ab833fd791efb9092d19d3adbe1edcf8d59
Jaume Ferrarons <ckroxigor@gmail.com>
2014-08-13 16:20:04 +0200

    Dynamic average

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 var map;
+//We have 6 layer all of them initially visible
+var layers_state = {
+	"Non-rated" : {state : true, name: null},
+	"1 star" : {state : true, name: 1},
+	"2 stars" : {state : true, name: 2},
+	"3 stars" : {state : true, name: 3},
+	"4 stars" : {state : true, name: 4},
+	"5 stars" : {state : true, name: 5},
+};
+var aggDataByStars = {};
+var curAvg = -1;
 
 $(function () {
 	console.log("loading...");
@@ -54,6 +65,26 @@ function init(hotels) {
 	for (l in layers) {
 		map.addLayer(layers[l]);
 	}
+
+	//Connect events
+	map.on('overlayadd', function(e) {
+		changeLayerState(e.name, true);
+	});
+	map.on('overlayremove', function(e) {
+		changeLayerState(e.name, false);
+	});
+	map.on('popupopen', function (e) {
+		//Get the source
+		var src = e.popup._contentNode.innerHTML;
+		src = $("<div>" + src + "</div>");
+		//Get the current score
+		var score = src.find(".score_info").data("score");
+		var dt = getDTScoreHTML(score - curAvg);
+		//Update the inner HTML
+		src.find(".diffGrade").replaceWith(dt)
+		//Set the HTML for the popup
+		e.popup.setContent(src.html());
+	});
 }
 
 //Returns the layers object wuth nicer names :)
@@ -80,16 +111,36 @@ function renameLayers (layers) {
 //Returns the average score of the list of given hotels
 function avgScore(hotels) {
 	var res = 0;
-	for (var h in hotels) res += hotels[h]["review_score"];
-	return res/hotels.length;
+	aggDataByStars = {
+		null : {sum: 0, count: 0},
+		1 : {sum: 0, count: 0},
+		2 : {sum: 0, count: 0},
+		3 : {sum: 0, count: 0},
+		4 : {sum: 0, count: 0},
+		5 : {sum: 0, count: 0}
+	};
+	for (var h in hotels) {
+		aggDataByStars[hotels[h]['stars']].sum += hotels[h]["review_score"];
+		aggDataByStars[hotels[h]['stars']].count += 1;
+		res += hotels[h]["review_score"];
+	}
+	curAvg = res/hotels.length;
+	return curAvg;
+}
+
+//Generates the HTML code the rating in colors
+function getDTScoreHTML(dt) {
+	dt = Number(dt).toPrecision(3);
+	if (dt >= 0) return "<span class='diffGrade' style='color:green;'><b>+" + dt + "</b></span>";
+	else return "<span class='diffGrade' style='color:red'><b> " + dt + "</b></span>";
+
 }
 
 //Returns the HMTL pop up for the given hotel
 function preparePopUp(hotel, mean_score) {
 	//Prepare score
-	dt_score = Number(hotel["review_score"] - mean_score).toPrecision(3);
-	if (dt_score >= 0) dt_score = "<span style='color:green;'><b>+" + dt_score + "</b></span>";
-	else dt_score = "<span style='color:red'><b> " + dt_score + "</b></span>";
+	dt_score = hotel["review_score"] - mean_score;
+	dt_score = getDTScoreHTML(dt_score);
 	//Prepare stars
 	stars = hotel["stars"];
 	if (stars == null) stars = "";
@@ -97,9 +148,37 @@ function preparePopUp(hotel, mean_score) {
 	else stars = " - " + stars  + " stars";
 	//Prepare pop-up
 	var res = "";
-	res += "<b>" + hotel["name"] + "</b>" + stars + " (" + hotel["review_score"]  + ")" + dt_score + "<br>";
+	res += "<b>" + hotel["name"] + "</b>" + stars + "<span class='score_info' data-score='" + hotel["review_score"] + "'> ("  + hotel["review_score"]  + ") " + dt_score + "</span><br>";
 	res += "<i>Address</i>: " + hotel["hotel_address"] + "<br>";
 	res += "<b>Good</b>: " + hotel["top_good_words"].split("##").join(", ") + "<br>";
 	res += "<b>Bad</b>: " + hotel["top_bad_words"].split("##").join(", ") + "<br>";
 	return res;
+}
+
+//Change the state of the given layer, on == True when shown, otherwise false
+function changeLayerState(name, on) {
+	layers_state[name].state = on;
+	updateLayers()
+}
+
+//Update the displayed mean and differences
+function updateLayers() {
+	//Compute current average
+	var sum = 0;
+	var count = 0;
+	var l;
+	for (l in layers_state) {
+		if (layers_state[l].state) {
+			var tmp = aggDataByStars[layers_state[l].name];
+			sum += tmp.sum;
+			count += tmp.count;
+		}
+	}
+	if (count > 0) curAvg = sum/count;
+	else curAvg = 0;
+	//Update current popup (if any)
+	var score = $(".score_info").data("score");
+	var dt = getDTScoreHTML(score - curAvg);
+	$(".diffGrade").replaceWith(dt)
+
 }
